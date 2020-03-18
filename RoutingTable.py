deebc3d935245e972266c6f54c40b0e64cb42278
Youre Name <rbe72@uclive.ac.nz>
2020-03-18 14:30:08 +1300

    Outlined Basic skeleton to RoutingTable class.

---
+++ RoutingTable.py
@@ -0,0 +1,7 @@
+"""
+This is the RoutingTable class for the COSC364 Assignment.
+"""
+class RoutingTable:
+    def __init__(self):
+        """Initialise starting properties"""
+        self.table = {}
