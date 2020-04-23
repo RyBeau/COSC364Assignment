e7a7138f4bca8022586c556f9c02c45369c9b843
Ryan Beaumont <ryan.beaumont1@gmail.com>
2020-04-23 21:19:08 +1200

    Testing Purposes

--- socket_tester.py
+++ socket_tester.py
@@ -4,7 +4,7 @@ from time import sleep
 test_socket = socket(AF_INET, SOCK_DGRAM)
 test_socket.bind((gethostname(), 5002))
 while True:
-    test_socket.sendto(bytes("Hello World 1", 'utf-8'), (gethostname(), 6110))
+    test_socket.sendto(bytes("Hello World 1", 'utf-8'), (gethostname(), 2001))
     sleep(1)
     test_socket.sendto(bytes("Hello World 2", 'utf-8'), (gethostname(), 6201))
     sleep(1)
