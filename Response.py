9b96c47209ad1836b9e58d8a6101e88c220ea45e
Kazuki Burrows <kazuiscool@gmail.com>
2020-04-25 00:35:22 +1200

    Titied code with better comments. Trigged updates now can be called with a parameter. Fixed buggs and tested. Ip_addr still needs to be changed to router id.

--- Response.py
+++ Response.py
@@ -4,174 +4,184 @@ import math
 #For regular 30 second response and triggered updates messages
 
 
-"""Form a response message packet and return a list of 'bytearray/s' s of packet back to the caller"""
-class ResponseSend:
+class ResponseSend():
     
     command = 2
     version = 1
-    must_be_zero = 0
-    
-    f = Format.Format()
+    must_be_zero = 0    
     
     
-    
-    """Return a RTE in integer format"""
-    def createRTE(self, ip_addr, metric):
-        f = self.f
+    """30 second unsolicited message. Returns message/s of the entire routing table"""
+    def unsolictedMessage(self):
         
-        # Get list of each element for packet, e.g addr_family_id_list = [0, 1] for address family id = 1
-        addr_family_id_list = f.formatAddrFamilyID(2)
-        must_be_zero16_list = f.formatMustBeZero16(0)
-        ip_addr_list = f.formatIP(ip_addr)
-        must_be_zero32_list = f.formatMustBeZero32(0)
-        metric_list = f.formatMetric(metric)
+        routingTable = RoutingTable.RoutingTable().table                            # The routing table(dictionary)
+        #routingTable = {1234: (1, 2, 12, "c"), 5678: (5, 6, 1, "c")}
+        
+        max_RTE_in_message = 25                                                     # Max number of RTE's that can fit in a single message
+        RTE_count = len(routingTable)                                               # Number of RTE's in routing table
+        message_count = 1 + (RTE_count // max_RTE_in_message)                       # Number of messages required to send entire routing table
+        
+        if RTE_count == 0:
+            # Do something
+            pass
+    
+    
+    
+        list_of_messages = []                                                       # Contains message/s in bytearray form that will be returned to the caller
         
         
-        # Concatonate lists to a single list(The size of the list is constant)
-        RTE_byte_list = (addr_family_id_list + 
+        
+        while len(list_of_messages) < message_count:                                # Loop until all messages have been created
+            message_byte_size, RTE_count = self.calculateMessageByteSize(RTE_count)        # Size of message in bytes
+            
+            message = bytearray(message_byte_size)
+            
+            # Message header
+            message[0] = self.command
+            message[1] = self.version
+            message[2] = self.must_be_zero
+            message[3] = self.must_be_zero
+            
+            
+            message_index = 4                                                       # Begin at index 4 of message since first 3 index's are the message header
+            
+            
+            for ip_addr in routingTable:
+                metric = routingTable[ip_addr][2]
+                
+                if (message_index >= message_byte_size):                            # If message_index is out of range in message(bytearray) size
+                    break                                                           # then break loop to start a new message
+            
+                
+                RTE = self.encodeRTE(ip_addr, metric)                               # Format the RTE to be appended to the message(bytearray) which is done on the next line
+                message, message_index = self.addToMessage(message, message_index, RTE)
+    
+    
+            # end of for loop
+            list_of_messages.append(message)                                        # Append message(bytearray) to list_of_messages after message is encoded
+
+
+        #end of while loop
+        return list_of_messages
+
+
+
+
+
+    """Make a triggered message/s. Param flagged_RTE_list is a list of flagged("c") keys. Returns message/s in a list"""
+    def triggerdMessage(self, flagged_RTE_list):
+        
+        routingTable = RoutingTable.RoutingTable().table
+        #routingTable = {1234: (1, 2, 3, "c"), 5678: (5, 6, 7, "c")}                # just a test routing table
+        
+        
+        max_RTE_in_message = 25                                                     # Max number of RTE's that can fit in a single message
+        RTE_count = len(routingTable)                                               # Number of RTE's in routing table
+        message_count = 1 + (RTE_count // max_RTE_in_message)                       # Number of messages required to send entire routing table
+        
+        list_of_messages = []                                                       # Contains message/s in bytearray form that will be returned to the caller
+        
+
+        
+        while len(list_of_messages) < message_count:                                # Loop until all messages have been created
+            message_byte_size, RTE_count = self.calculateMessageByteSize(RTE_count)        # Size of message in bytes
+            
+            message = bytearray(message_byte_size)
+            
+            # Message header
+            message[0] = self.command
+            message[1] = self.version
+            message[2] = self.must_be_zero
+            message[3] = self.must_be_zero
+            
+            
+            message_index = 4                                                       # Begin at index 4 of message since first 3 index's are the message header
+            
+            for ip_addr in flagged_RTE_list:
+                metric = routingTable[ip_addr][2]
+                
+                if (message_index >= message_byte_size):                            # If message_index is out of range in message(bytearray) size
+                    break                                                           # then break loop to start a new message
+                
+                
+                RTE = self.encodeRTE(ip_addr, metric)                               # Format the RTE to be appended to the message(bytearray) which is done on the next line
+                message, message_index = self.addToMessage(message, message_index, RTE)
+                
+            
+            # end of for loop
+            list_of_messages.append(message)                                        # Append message(bytearray) to list_of_messages after message is encoded
+
+
+        #end of while loop
+        return list_of_messages            
+        
+        
+
+
+
+
+    """
+    
+    HELPER FUNCTIONS BELOW
+    
+    """
+
+
+    """Calculate the size of message depending on how many RTE's are in the routing table, returns number of bytes"""
+    def calculateMessageByteSize(self, RTE_count):
+        max_message_byte_size = 504                                                 # Max number of bytes for a single message
+        message_header_size = 4                                                     # Message header size(number of bytes)
+        RTE_size = 20                                                               # Single RTE size(number of bytes)
+        
+        message_byte_size = message_header_size + (RTE_count * RTE_size)
+        
+        if message_byte_size > max_message_byte_size:                               # If RTE count > 25, 
+            return max_message_byte_size, RTE_count-25                              # then return max message byte size and RTE_count - 25(since 25 are going to be sent)
+        
+        
+        return message_byte_size, 0                                                 # Else, return calculated message byte size and 0(since there will be no remaining RTE's to send after)
+    
+    
+    
+    
+    
+    
+    """Encode a single RTE, formatted to be added to the message"""
+    def encodeRTE(self, ip_addr, metric):
+        f = Format.Format()
+        
+        # Each property is returned in a list, each value in the list represents a byte in the message(bytearray). e.g [1, 2], i=index(byte) in message, message[i] = 1 and message[i+1] = 2
+        addr_family_id_list = f.formatAddrFamilyID(2)                               # Format addres-family-id property. Param is the property value to be formatted
+        must_be_zero16_list = f.formatMustBeZero16(0)                               # Format must-be-zero(16 bits) property. Param is the property value to be formatted
+        ip_addr_list = f.formatIP(ip_addr)                                          # Format ip-address property. Param is the property value to be formatted
+        must_be_zero32_list = f.formatMustBeZero32(0)                               # Format must-be-zero(32 bits) property. Param is the property value to be formatted
+        metric_list = f.formatMetric(metric)                                        # Format metric property. Param is the property value to be formatted
+        
+        # Concatonate property lists to a single list
+        RTE = (addr_family_id_list + 
                          must_be_zero16_list + 
                          ip_addr_list + 
                          must_be_zero32_list + 
                          must_be_zero32_list + 
                          metric_list)
         
+        return RTE
+    
+    
+    
+    
+    
+    """Append RTE to message(bytearray) and return new message(with appended RTE) and message_index(next index free space in message for the next RTE to begin at)"""
+    def addToMessage(self, message, message_index, RTE):
         
-        return RTE_byte_list
-
-
-
-
-    """Add RTE in packet form to packet. Returns packet & packet index(to keep track of what bytes are occupied in packet('byteArray')"""
-    def appendToPacket(self, packet, packet_index, RTE_byte_list):
-        
-        i = 0
-        while i < len(RTE_byte_list):
-            packet[packet_index] = RTE_byte_list[i]     # 'calculatePacketByteSize()' ensures no index range error
+        i = 0                                                                       # Index for RTE, since RTE is a list, each value in the list represents a byte in the message(bytearray)
+        while i < len(RTE):
+            message[message_index] = RTE[i]                                         # Append RTE property value to message
             
-            packet_index += 1
-            i += 1        
+            message_index += 1
+            i += 1
         
-        return packet, packet_index
-    
-    
-    
-    
-    
-    """Calculate the size of packet depending on how many RTE's are in routing table, returns number of bytes"""
-    def calculatePacketByteSize(self, expected_num_packets, current_num_packets, RTE_total):
-        max_packet_byte_size = 504      # max number of bytes for a single packet
-        packet_header_size = 4          # packet header size(number of bytes)
-        RTE_size = 20                   # RTE size(number of bytes)
-        
-        packet_byte_size = packet_header_size + (RTE_total * RTE_size)
-        
-        if packet_byte_size > max_packet_byte_size:     # if true, means multiple packets will be generated
-            return max_packet_byte_size
-        
-        
-        return packet_byte_size
-    
-    
-    
-    
-    
-    # THIS WILL NOW ONLY BE FOR THE 30 SECOND UPDATE PACKET
-    """Initial function to call. Returns a list of packet/s depending on how many RTE's are in routing table"""
-    def makeUnsolictedUpdate(self):
-        
-        routingTable = RoutingTable.RoutingTable().table
-        
-        RTE_total = len(routingTable)         # Number of RTE's in 'RoutingTable' class
-        
-        num_packets = math.ceil(float(RTE_total)/ float(max_num_packet_RTE))        # Number of packet required to send all RTE's
-        
-        packet_list = []        # List of packet/s to return
-        
-        
-        while len(packet_list) < num_packets:
-            packet_byte_size = calculatePacketByteSize(num_packets, len(packet_list), RTE_total)
-            
-            packet = bytearray(packet_byte_size)
-            
-            #packet header
-            packet[0] = self.command
-            packet[1] = self.version
-            packet[2] = self.must_be_zero
-            packet[3] = self.must_be_zero
-            
-            
-            packet_index = 4
-            
-            for ip_addr, metric, _ in routingTable.items():
-                if (packet_index < packet_byte_size):       #when packet is at max size, break to start new packet
-                    break
-                
-                RTE_byte_list = self.createRTE(ip_addr, metric)
-                packet, packet_index = self.appendToPacket(packet, packet_index, RTE_byte_list)
-                
-                
-                
-            packet_list.append(packet)
-        
-        
-        return packet_list
-        
-        
-        
-        
-        
-    
-        
-        
-        
-    #ONLY MAKES TRIGGERED UPDATES
-    def makeTriggeredUpdate(self):
-        
-        routingTable = RoutingTable.RoutingTable().table
-        
-        RTE_list = []
-        
-        # Get RTE's that have 'c' flag
-        for key, _, flag in routingTable.items():
-            if (flag == "c"):
-                RTE_list.append(key)
-                
-        
-        RTE_total = len(RTE_list)
-        num_packets = math.ceil(float(RTE_total)/ float(max_num_packet_RTE))        # Number of packet required to send all RTE's
-        
-        packet_list = []        # List of packet/s to return        
-        
-        
-        
-        while len(packet_list) < num_packets:
-            packet_byte_size = calculatePacketByteSize(num_packets, len(packet_list), RTE_total)
-            
-            packet = bytearray(packet_byte_size)
-        
-            #packet header
-            packet[0] = self.command
-            packet[1] = self.version
-            packet[2] = self.must_be_zero
-            packet[3] = self.must_be_zero
-            
-            packet_index = 4
-            
-            for RTE in RTE_list:
-                if (packet_index < packet_byte_size):       #when packet is at max size, break to start new packet
-                    break                
-                
-                ip_addr = RTE
-                metric = routingTable[RTE][0]
-                
-                RTE_byte_list = self.createRTE(ip_addr, metric)
-                packet, packet_index = self.appendToPacket(packet, packet_index, RTE_byte_list)
-                
-                
-            packet_list.append(packet)                
-        
-        
-        return packet_list
+        return message, message_index
         
         
 
@@ -318,6 +328,37 @@ class ResponseReceive():
 #packet[22] = metric_3
 #packet[23] = metric_4
 
+##Header
+#command = int('00000010', 2)
+#version = int('00000001', 2)
+#zero1_16_1 = int('00000000', 2)
+#zero1_16_2 = int('00000000', 2)
+
+##RTE
+#addr_family_id_1 = int('00000000', 2)
+#addr_family_id_2 = int('00000011', 2)
+#zero2_16_1 = int('00000000', 2)
+#zero2_16_2 = int('00000000', 2)
+
+#ip1 = int('11001011', 2)
+#ip2 = int('00100100', 2)
+#ip3 = int('10110011', 2)
+#ip4 = int('01010011', 2)
+
+#zero1_32_1 = int('00000000', 2)
+#zero1_32_2 = int('00000000', 2)
+#zero1_32_3 = int('00000000', 2)
+#zero1_32_4 = int('00000000', 2)
+
+#zero2_32_1 = int('00000000', 2)
+#zero2_32_2 = int('00000000', 2)
+#zero2_32_3 = int('00000000', 2)
+#zero2_32_4 = int('00000000', 2)
+
+#metric_1 = int('00000000', 2)
+#metric_2 = int('00000000', 2)
+#metric_3 = int('00000000', 2)
+#metric_4 = int('00000011', 2)
 
 #packet[24] = addr_family_id_1
 #packet[25] = addr_family_id_2
@@ -342,3 +383,23 @@ class ResponseReceive():
 
 
 #print(r.readResponse(packet))
+
+
+
+## test unsolicited message
+#s = ResponseSend()
+#print(s.unsolictedMessage())
+
+
+## test triggered message
+#s = ResponseSend()
+#print(s.triggerdMessage([1234, 5678]))
+
+
+## test reading received message
+#r = ResponseReceive()
+#a = r.readResponse(bytearray(b'\x02\x01\x00\x00\x00\x02\x00\x00\x00\x00\x04\xd2\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x03\x00\x02\x00\x00\x00\x00\x16.\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x07'))
+#print(a)
+
+
+
