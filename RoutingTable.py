c0950b96e6ed45d3ef7f232339ad9883459f2a17
Ryan Beaumont <ryan.beaumont1@gmail.com>
2020-04-27 20:28:20 +1200

    Changed format of update function arguments to better format (rip_entries, next_hop_id, link_metric)

--- RoutingTable.py
+++ RoutingTable.py
@@ -20,18 +20,18 @@ class RoutingTable:
             table_string += "{}: Next Hop ID {}, Metric: {}, Flag {}\n".format(key, self.table[key][1], self.table[key][2], self.table[key][3])
         return "Routing Table\n" + table_string
 
-    def update(self, rip_entries, link_metric):
+    def update(self, rip_entries, next_hop_id, link_metric):
         """
         Expected RIP entry format
-        (destination_id, next_hop_id, metric)
+        (destination_id, metric)
         """
         for entry in rip_entries:
-            if not str(entry[0]) in self.table.keys() and entry[2] + link_metric < 16:
-                self.table[str(entry[0])] = (entry[0], entry[1], entry[2] + link_metric, "c")
-            elif self.table[str(entry[0])][2] > entry[2] + link_metric:
-                self.table[str(entry[0])] = (entry[0], entry[1], entry[2] + link_metric, "c")
-            elif self.table[str(entry[0])][1] == entry[1] and entry[2] == 16:
-                self.table[str(entry[0])] = (entry[0], entry[1], entry[2], "d")
+            if not str(entry[0]) in self.table.keys() and entry[1] + link_metric < 16:
+                self.table[str(entry[0])] = (entry[0], next_hop_id, entry[1] + link_metric, "c")
+            elif self.table[str(entry[0])][2] > entry[1] + link_metric:
+                self.table[str(entry[0])] = (entry[0], next_hop_id, entry[1] + link_metric, "c")
+            elif self.table[str(entry[0])][1] == next_hop_id and entry[1] == 16:
+                self.table[str(entry[0])] = (entry[0], next_hop_id, entry[1], "d")
 
     def get_entries(self, receiving_neighbour):
         """
@@ -68,19 +68,19 @@ if __name__ == "__main__":
     print("Intial Table:")
     print(routing_table)
     print("First Fill Test")
-    test_entries1 = [(1, 2, 5), (3, 2, 10), (4, 2, 15), (5, 2, 1)]
-    routing_table.update(test_entries1, 0)
+    test_entries1 = [(1, 5), (3, 10), (4, 15), (5, 1)]
+    routing_table.update(test_entries1, 2, 0)
     print(routing_table)
     print("Updating table Test")
-    test_entries2 = [(1, 3, 1), (4, 3, 10), (5, 3, 5)]
-    routing_table.update(test_entries2, 0)
+    test_entries2 = [(1, 1), (4, 10), (5, 5)]
+    routing_table.update(test_entries2, 3, 0)
     print(routing_table)
     print("Set Unreachable Test")
-    test_entries3 = [(1, 3, 16), (4, 3, 16), (5, 3, 16)]
-    routing_table.update(test_entries3, 0)
+    test_entries3 = [(1, 16), (4, 16), (5, 16)]
+    routing_table.update(test_entries3, 3, 0)
     print(routing_table)
-    test_entries4 = [(1, 3, 1), (4, 3, 3)]
-    routing_table.update(test_entries4, 0)
+    test_entries4 = [(1, 1), (4, 3)]
+    routing_table.update(test_entries4, 3, 0)
     print(routing_table)
     print("Get entries test no SH needed")
     print(routing_table.get_entries(5))
