518ee5abdd5a1323380fb02d98dc97e095164b52
Ryan Beaumont <ryan.beaumont1@gmail.com>
2020-05-05 21:11:12 +1200

    Updated to error check the config file as specified in the assignment specification

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+from CustomExceptions import *
+
+CONFIG_FORMAT = "format should match:\n{Router_id}\ninput ports {input port}, {input port},...\noutput ports {output port}-{metric}-{id},..."
 
 # convert id from string to int
 def convert_id(param_id):
@@ -5,129 +8,111 @@ def convert_id(param_id):
         converted_id = int(param_id)
         if 1 <= converted_id <= 64000:
             return converted_id
-    
-    except:
-        return False
-    
-    
+        else:
+            raise RouterException("Router id is out of range must be between 1 and 64000 inclusive")
+    except RouterException:
+        raise
+    except Exception:
+        raise RouterException("Error converting router id\n" + CONFIG_FORMAT)
+
+
 # Return a list of input ports as integers
 def convert_input(param_input_ports):
     port_list = []
     try:
         for port in param_input_ports:
             converted_port = int(port)
-            
-            if 1024 <= converted_port <= 64000:
-                port_list.append(converted_port)
-                
-            else:
-                return False
-            
+
+            if not (1024 <= converted_port <= 64000):
+                raise RouterException("Input port out of range must be between 1024 and 64000 inclusive")
+
+            if converted_port in port_list:
+                raise RouterException("Input ports can only be used once")
+
+            port_list.append(converted_port)
+
         return port_list
-    
-    except:
-        return False    
-    
-    
+
+    except RouterException:
+        raise
+    except Exception:
+        raise RouterException("Unable to convert input ports\n" + CONFIG_FORMAT)
+
+
 # Return a list of output ports as integers
-def convert_output(param_output_ports):
+def convert_output(param_output_ports, input_ports):
     port_list = []
     try:
         for port in param_output_ports:
-            
+
             port = port.split('-')
             router_port = int(port[0])
             router_metric = int(port[1])
             router_id = int(port[2])
-            
-            if not(1024 <= router_port <= 64000):
-                return False
-            
-            if router_metric < 0:
-                return False
-            
-            if not(1 <= router_id <= 64000):
-                return False
-            
-            
+
+            if not (1024 <= router_port <= 64000):
+                raise RouterException("Output port out of range must be between 1024 and 64000 inclusive")
+
+            if router_metric < 0 and router_metric < 16:
+                raise RouterException("Route metric out of range must be between 1 and 16 inclusive")
+
+            if not (1 <= router_id <= 64000):
+                raise RouterException("Neighbour router ID out of range must be between 1 and 64000 inclusive")
+
+            if router_port in input_ports:
+                raise RouterException("Output port is also an input port")
+
+            if router_port in [p[0] for p in port_list]:
+                raise RouterException("Output ports can only be used once")
+
             port_tuple = tuple([router_port, router_metric, router_id])
             port_list.append(port_tuple)
-            
-            
+
         return port_list
-    
-    except:
-        return False
-    
+    except RouterException:
+        raise
+    except Exception:
+        raise RouterException("Could not convert output ports\n" + CONFIG_FORMAT)
 
 
 # Main
 def router_config(filename):
     try:
         file = open(filename, 'r')
-        
+
         print("\'%s\' file exists" % filename)
-        
-    except:
-        print("Config file cannot be found.")
-        return
 
-    
-    
-    router_id = None;
-    input_ports = []
-    output_ports = []
-    
-    lines = file.readlines()
-    
-    i = 0
-    while i < len(lines): # filters out comments and new lines
-        line = lines[i]
-        
-        if line == '\n':
-            lines.pop(i)
-        
-        elif line[0] == "#":
-            lines.pop(i)
-            
+    except Exception:
+        raise RouterException("File not found")
+
+    else:
+        lines = file.readlines()
+
+        i = 0
+        while i < len(lines):  # filters out comments and new lines
+            line = lines[i]
+
+            if line == '\n':
+                lines.pop(i)
+
+            elif line[0] == "#":
+                lines.pop(i)
+
+            else:
+                i += 1
+
+        filtered_lines = [line[:-1] if line[-1] == "\n" else line for line in lines]
+
+        try:
+            router_id = convert_id(filtered_lines[0])
+            input_ports = convert_input(filtered_lines[1][12:].split(", "))
+            output_ports = convert_output(filtered_lines[2][13:].split(", "), input_ports)
+
+        except RouterException:
+            raise
+        except Exception:
+            raise RouterException("Error with config file " + CONFIG_FORMAT)
         else:
-            i += 1
-            
-    
-    filtered_lines = [line[:-1] if line[-1] == "\n" else line for line in lines]
-    
-    
-    for line in filtered_lines:
-        if len(line) == 1: # get id
-            router_id = line
+            return router_id, input_ports, output_ports
 
-        elif line[:11] == "input-ports": # get input ports
-            input_ports = line[12:].split(', ')
-            
-        elif line[:12] == "output-ports": # get output ports
-            output_ports = line[13:].split(', ')
-            
-    
-    
-    router_id = convert_id(router_id)
-    if router_id == False:
-        print("Problem converting router id.")
-        return
-    
-    input_ports = convert_input(input_ports)
-    if input_ports == False:
-        print("Problem converting input ports")
-        return
-    
-    output_ports = convert_output(output_ports)
-    if output_ports == False:
-        print("Problem converting output ports")
-        return
-    
-    
-    
-    return router_id, input_ports, output_ports
-
-
-
-#router_config("router1.txt")
+# router_config("router1.txt")
